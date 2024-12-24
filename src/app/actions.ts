483f39f098234353a783a2cf8f6e4e89fc884628
Ritik <raj76941@gmail.com>
2024-12-24 23:19:01 +0530

    added server side validation

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,24 +1,34 @@
 "use server";
 
-import { connectDB } from "@/lib/utils";
+import { connectDB, formatErrors } from "@/lib/utils";
 import { Todo } from "./models/todos";
 import { revalidateTag } from "next/cache";
-import { FormType } from "@/components/AddItemForm";
+import { z } from "zod";
+import { formSchema, FormType } from "@/lib/types";
 
-export async function addTodos(_prevState: any, data: FormData) {
+export async function addTodos(data: FormType) {
+  const parseData = formSchema.safeParse(data);
+  if (!parseData.success) {
+    return formatErrors<FormType>(parseData.error.errors);
+  }
+
   await connectDB();
 
-  const todo = new Todo({ title: data.get("title") });
+  const todo = new Todo({ title: data.title });
   await todo.save();
 
   revalidateTag("todos");
   return { success: true };
 }
 
-export async function editTodos(_prevState: any, data: FormData, _id: string) {
+export async function editTodos(data: FormType, _id: string) {
+  const parseData = formSchema.safeParse(data);
+  if (!parseData.success) {
+    return formatErrors<FormType>(parseData.error.errors);
+  }
   await connectDB();
 
-  await Todo.findByIdAndUpdate(_id, { title: data.get("title") });
+  await Todo.findByIdAndUpdate(_id, { title: data.title });
 
   revalidateTag("todos");
   return { success: true };
